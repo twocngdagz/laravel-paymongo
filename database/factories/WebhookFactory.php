60e6d062f46b7fbed2db16a858b3e84928a6b33c
twocngdagz <twocngdagz@users.noreply.github.com>
2023-01-29 13:59:54 +0000

    Fix styling

--- database/factories/WebhookFactory.php
+++ database/factories/WebhookFactory.php
@@ -5,7 +5,6 @@ namespace Twocngdagz\LaravelPaymongo\Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Twocngdagz\LaravelPaymongo\Models\Webhook;
 
-
 class WebhookFactory extends Factory
 {
     protected $model = Webhook::class;
@@ -13,9 +12,8 @@ class WebhookFactory extends Factory
     public function definition()
     {
         return [
-            'webhook_id' => 'hook_' . faker()->uuid,
-            'event' => randomElement()
+            'webhook_id' => 'hook_'.faker()->uuid,
+            'event' => randomElement(),
         ];
     }
 }
-
