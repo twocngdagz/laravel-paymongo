42949f87c91daf8286dcd82279ce5746aa688129
Mederic Roy Beldia <twocngdagz@yahoo.com>
2023-01-29 21:47:24 +0800

    update Pest for setting up configuration for spatie webhook client  and migration files

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Twocngdagz\LaravelPaymongo\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
+uses(RefreshDatabase::class);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,10 @@
 namespace Twocngdagz\LaravelPaymongo\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Schema;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Spatie\LaravelData\LaravelDataServiceProvider;
+use Spatie\WebhookClient\WebhookClientServiceProvider;
 use Twocngdagz\LaravelPaymongo\LaravelPaymongoServiceProvider;
 
 class TestCase extends Orchestra
@@ -21,18 +23,25 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [
+            WebhookClientServiceProvider::class,
             LaravelPaymongoServiceProvider::class,
             LaravelDataServiceProvider::class,
+
         ];
     }
 
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
+        config()->set('webhook-client.configs.0.process_webhook_job', \Twocngdagz\LaravelPaymongo\Jobs\ProcessWebhookFromPaymongo::class);
+        config()->set('webhook-client.configs.0.signature_validator', \Twocngdagz\LaravelPaymongo\SignatureValidator\PaymongoSignatureValidator::class);
+        config()->set('webhook-client.configs.0.signature_header_name', 'Paymongo-Signature');
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_laravel-paymongo_table.php.stub';
+        Schema::dropAllTables();
+
+        $migration = include __DIR__.'/../database/migrations/create_laravel-paymongo_table.php';
         $migration->up();
-        */
+        $webclient_migration = include __DIR__.'/../vendor/spatie/laravel-webhook-client/database/migrations/create_webhook_calls_table.php.stub';
+        $webclient_migration->up();
     }
 }
